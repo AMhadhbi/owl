939a6e87c6a770ea5853a6f029ea517a15a416c6
Achraf Mhadhbi <mhadhbi.achref@gmail.com>
2024-11-17 18:00:26 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'OWL TODO List App',
+    'version':'1.0',
+    'sequence': -1,
+    'description':"""OWL TODO List App""",
+    'category': 'OWL',
+    'depends' : ['base'],
+    'data' :[],
+    'installable': True,
+    'application':True,
+    'assets': {
+        'web.assets_backend' :[],
+    }
+
+    
+}
